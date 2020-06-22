126a5f540fc93db1f22858ee0e803af61c843311
Adrian Lopez <ajlopez17@miners.utep.edu>
2020-06-22 13:08:20 -0600

    Added back tokenizer.h

--- src/tokenizer.h
+++ src/tokenizer.h
@@ -1,27 +1,48 @@
 #ifndef _TOKENIZER_
 #define _TOKENIZER_
 
-
 /* Return true (non-zero) if c is a whitespace characer
    ('\t' or ' ').  
    Zero terminators are not printable (therefore false) */
 int space_char(char c);
-
+  char c;
+  if (c == ' ' || c == '\t')
+    return 1;
+  return 0;
+        
 /* Return true (non-zero) if c is a non-whitespace 
    character (not tab or space).  
    Zero terminators are not printable (therefore false) */ 
 int non_space_char(char c);
+  char c;
+  if (c != ' ' || c != '\t')
+    return 1;
+  return 0;
 
 /* Returns a pointer to the first character of the next 
    space-separated word in zero-terminated str.  Return a zero pointer if 
    str does not contain any words. */
-char word_start(char *str); 
-
+char *word_start(char *str);
+  
+  
 /* Returns a pointer terminator char following *word */
 char *word_terminator(char *word);
 
 /* Counts the number of words in the string argument. */
 int count_words(char *str);
+  int count = 0;
+  int len = 0;
+  if (str == NULL)
+    return 0;
+
+  while (*str){
+    if (*str == ' ' || *str == '\t')
+      len = 0;
+    else if (++len == 1)
+      count++;
+    str++;
+  }
+  return count;
 
 /* Returns a fresly allocated new zero-terminated string 
    containing <len> chars from <inStr> */
